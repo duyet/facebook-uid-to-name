1330b25e955331cbfaed2c638bff19a4f7c30e48
Van-Duyet Le <me@duyetdev.com>
2016-12-28 00:47:54 +0700

    Sample, TODO: Scale

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 var graph = require('fbgraph');
+var fs = require('fs');
 var config = require('./config');
 
 graph.setAccessToken(config.static_access_token);
 
-graph.get('/me', function (err, res) {
-	console.log(res)
+fs.readFile('./data/user_ids.sample.txt', function(err, data) {
+	if (err) throw err;
+
+	var array = data.toString().split("\n");
+    for (i in array) {
+    	graph.get('/' + array[i], function (err, res) {
+			console.log(res)
+		})
+    }
 })
