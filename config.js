2213c95b01bced045d4e90726d3fd1da03fe87fd
Van-Duyet Le <me@duyetdev.com>
2016-12-28 08:03:20 +0700

    Update with sample data, with gender

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
 	'appId'  : "your appID",
 	'secret' : "your app secret",
 	'cookie' : true,
-	'static_access_token': 'EAACEdEose0cBAIEZB95SqNt28ozWwbN0HpXZCdLmiqv4H4NQRmHReti4m1k9wSHkOIZCaMf9uR1gsmyGEDII7kWhvAWYpWX40VlFMDB2ZBWIGWABeDXqDnujH7e8ZCF5K1n3nOksSlLEMZBzZAOzSjU7mVjSItHaBQxSZBhZCFVjL756NIVkXWxWW'
+	'static_access_token': 'EAACEdEose0cBAOvufbBP49KgMTlov8wnY3ZAS7hOwxTTEg9nTbEGVx5P8roP6ZAZBcP4mB1mDw0hPW8ds7L8ZBSd5pRYzZAZA01u9ynRxQhKzVhxwcYsjXjNT3SbLvhE78GLbT6OtXlIPumnCcnnHELbfNWCjyYhsJXUktKpruf3MlcKGSA6CN9t5VoZBTvkBIZD'
 }
--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 var graph = require('fbgraph');
 var fs = require('fs');
 var config = require('./config');
-
 graph.setAccessToken(config.static_access_token);
 
-fs.readFile('./data/user_ids.sample.txt', function(err, data) {
-	if (err) throw err;
+var inputFile = './data/facebook_5k.txt';
+var outputFile = './data/user_ids.with_name.txt';
 
-	var array = data.toString().split("\n");
-    for (i in array) {
-    	graph.get('/' + array[i], function (err, res) {
-			console.log(res)
-		})
-    }
-})
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream(inputFile)
+});
+
+function getUserNameFromUserId(user_id, success, error) {
+	graph.get('/' + user_id, function (err, res) {
+		if (err && error) error(err);
+		else success(res);
+	});
+}
+
+lineReader.on('line', function (line) {
+  console.log('Line from file:', line);
+  var user_id = line.split(',');
+
+  getUserNameFromUserId(user_id[0], function(res) {
+  	console.log(res, user_id[1])
+  	var new_line = res['id'] + ',' + res['name'] + ',' + user_id[1] + '\n';
+  	fs.appendFile(outputFile, new_line, function(err) {
+  		if (err) console.log('Error when write result', err);
+  		else console.log('OK', new_line);
+  	});
+
+  }, function(err) {
+  	console.log("Error: ", err)
+  })
+});
